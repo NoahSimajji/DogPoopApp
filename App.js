0b9155f33d1b1b450dabda01513636e27cbc8a0d
“Noah” <noah@simajji.com>
2022-02-28 14:56:26 +0800

    Moved tabs screen to HomeStack.js

--- App.js
+++ App.js
@@ -1,57 +1,11 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Contents from "./screens/Contents.js";
-import Profile from "./screens/Profile.js";
-import Settings from "./screens/Settings.js";
 import Login from "./screens/Login.js";
 import React from "react";
-import {
-  MaterialCommunityIcons,
-  Ionicons,
-  AntDesign,
-} from "@expo/vector-icons";
+import HomeScreen from "./navigate/HomeStack";
 import { LoginPhoto } from "./components/enlargeImage";
 
 const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
-
-function HomeScreen() {
-  return (
-    <Tab.Navigator initialRouteName="Settings">
-      <Tab.Screen
-        name="Home"
-        component={Contents}
-        options={{
-          tabBarLabel: "Home",
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="home" color={color} size={30} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Profile"
-        component={Profile}
-        options={{
-          tabBarLabel: "Profile",
-          tabBarIcon: ({ color }) => (
-            <AntDesign name="profile" color={color} size={30} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Settings"
-        component={Settings}
-        options={{
-          tabBarLabel: "Settings",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="ios-settings" color={color} size={30} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  );
-}
 
 function App() {
   return (
@@ -65,9 +19,8 @@ function App() {
         <Stack.Screen
           options={{
             headerLeft: () => <LoginPhoto size={"small"} />,
-            headerTitle: "My Doggo App",
+            headerTitle: " My Doggo",
             headerTitleStyle: {
-              fontFamily: "AmericanTypewriter-Bold",
               fontSize: 22,
             },
           }}
---
+++ navigate/HomeStack.js
@@ -0,0 +1,50 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  MaterialCommunityIcons,
+  Ionicons,
+  AntDesign,
+} from "@expo/vector-icons";
+import Contents from "../screens/Contents";
+import Profile from "../screens/Profile.js";
+import Settings from "../screens/Settings.js";
+
+const Tab = createBottomTabNavigator();
+
+function HomeScreen() {
+  return (
+    <Tab.Navigator initialRouteName="Settings">
+      <Tab.Screen
+        name="Home"
+        component={Contents}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={30} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="profile" color={color} size={30} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={Settings}
+        options={{
+          tabBarLabel: "Settings",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="ios-settings" color={color} size={30} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+export default HomeScreen;
