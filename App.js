238a1f49189caa4c3aba215b4ebdfaec891a1bae
“Noah” <noah@simajji.com>
2022-03-06 16:32:21 +0800

    Added push notification and half way editing

--- App.js
+++ App.js
@@ -1,34 +1,40 @@
+import React from "react";
+
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+
 import Login from "./screens/Login.js";
-import React from "react";
 import HomeScreen from "./navigate/HomeStack";
+import RemotePushNotification from "./services/RemotePushNotification";
 import { LoginPhoto } from "./components/enlargeImage";
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          options={{ headerShown: false }}
-          name="Login"
-          component={Login}
-        />
-        <Stack.Screen
-          options={{
-            headerLeft: () => <LoginPhoto size={"small"} />,
-            headerTitle: " My Doggo",
-            headerTitleStyle: {
-              fontSize: 22,
-            },
-          }}
-          name="Home"
-          component={HomeScreen}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Login"
+            component={Login}
+          />
+          <Stack.Screen
+            options={{
+              headerLeft: () => <LoginPhoto size={"small"} />,
+              headerTitle: " My Doggo",
+              headerTitleStyle: {
+                fontSize: 22,
+              },
+            }}
+            name="Home"
+            component={HomeScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+      <RemotePushNotification />
+    </>
   );
 }
 
