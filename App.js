fabcd7c2d9357268b5dcb9353d323d2e47f2b46e
“Noah” <noah@simajji.com>
2022-03-26 23:48:07 +0800

    Connecting Store.js to App.js

--- App.js
+++ App.js
@@ -2,40 +2,44 @@ import React from "react";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { Provider } from "react-redux";
 
 import Login from "./screens/Login.js";
 import Register from "./screens/Register.js";
 import HomeScreen from "./navigate/HomeStack";
 import RemotePushNotification from "./services/RemotePushNotification";
 import { LoginPhoto } from "./components/enlargeImage";
+import Store from "./helpers/Store";
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Login">
-          <Stack.Screen
-            options={{ headerShown: false }}
-            name="Login"
-            component={Login}
-          />
-          <Stack.Screen name="Register" component={Register} />
-          <Stack.Screen
-            options={{
-              headerLeft: () => <LoginPhoto size={"small"} />,
-              headerTitle: " My Doggo",
-              headerTitleStyle: {
-                fontSize: 22,
-              },
-            }}
-            name="Home"
-            component={HomeScreen}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-      <RemotePushNotification />
+      <Provider store={Store}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen
+              options={{ headerShown: false }}
+              name="Login"
+              component={Login}
+            />
+            <Stack.Screen name="Register" component={Register} />
+            <Stack.Screen
+              options={{
+                headerLeft: () => <LoginPhoto size={"small"} />,
+                headerTitle: " My Doggo",
+                headerTitleStyle: {
+                  fontSize: 22,
+                },
+              }}
+              name="Home"
+              component={HomeScreen}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+        <RemotePushNotification />
+      </Provider>
     </>
   );
 }
