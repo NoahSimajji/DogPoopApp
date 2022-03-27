698ddb6618afbdc62ae12dfa409838c5a063d1c1
“Noah” <noah@simajji.com>
2022-03-27 12:12:52 +0800

    Clean up on Store.js

--- helpers/Store.js
+++ helpers/Store.js
@@ -1,7 +1,6 @@
 import { createStore } from "redux";
 
 const initialState = {
-  userDetails: {},
   accessToken: null,
   isShowProcessOverlay: false,
 };
@@ -11,11 +10,8 @@ const reducer = (state = initialState, action) => {
     case "update_is_show_process_overlay":
       return { ...state, isShowProcessOverlay: action.value };
 
-    // case REDUX.UPDATE_USER_DETAILS:
-    //   return { ...state, userDetails: action.value };
-
-    // case REDUX.UPDATE_ACCESS_TOKEN:
-    //   return { ...state, accessToken: action.value };
+    case "update_access_token":
+      return { ...state, accessToken: action.value };
   }
   return state;
 };
